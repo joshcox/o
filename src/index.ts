47c6d9fcaa4ed3c8c4ba7c2c11e6ee926232a5cf
Josh Cox <cox.josh.d@gmail.com>
2018-09-19 17:12:07 -0500

    Update: Lint corrections

--- src/index.ts
+++ src/index.ts
@@ -17,12 +17,12 @@ type Last_<T extends any[]> = {
 // Create the Composed function signature by creating a function type where
 // the input is the argument type of the last function and the output is the return type
 // of the first function
-type ComposeSignature<FS extends Array<(a: any) => any>> = 
-    (arg: ArgumentType<Last<FS>>) => 
-        FS extends [infer HEAD, ...any[]] 
-            ? HEAD extends (a: any) => any 
-                ? ReturnType<HEAD> 
-                : never 
+type ComposeSignature<FS extends Array<(a: any) => any>> =
+    (arg: ArgumentType<Last<FS>>) =>
+        FS extends [infer HEAD, ...any[]]
+            ? HEAD extends (a: any) => any
+                ? ReturnType<HEAD>
+                : never
             : never;
 
 // Validate passthrough/intermediate types
@@ -31,18 +31,18 @@ type Composed<FS extends Array<(a: any) => any>> = Composed_<FS, FS>;
 type Composed_<S extends Array<(a: any) => any>, T extends Array<(a: any) => any>> = {
     0: ComposeSignature<S>,
     // @ts-ignore
-    1: Head<Tail<T>> extends (a: any) => infer R 
-        ? ArgumentType<Head<T>> extends R 
-            ? Composed_<S, Tail<T>> 
+    1: Head<Tail<T>> extends (a: any) => infer R
+        ? ArgumentType<Head<T>> extends R
+            ? Composed_<S, Tail<T>>
             : never
-        : never 
+        : never
 }[T extends [any] ? 0 : 1];
 
 function compose(): <A>(arg: A) => A;
 function compose<F extends (arg: any) => any>(f: F): F;
-function compose<FS extends Array<(arg: any) => any>>(...fns: FS): Composed<FS>
+function compose<FS extends Array<(arg: any) => any>>(...fns: FS): Composed<FS>;
 function compose<FS extends Array<(arg: any) => any>>(...fns: FS) {
     return <A>(arg: A) => fns.reduce((a, f) => f(a), arg);
 }
 
-export { compose as o }
+export { compose as o };
